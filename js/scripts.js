c4741b9196d7a62824b8d8698162210f98bca166
Kimberly Huynh and Sara Hamilton <sara@>
2018-01-16 11:50:29 -0800

    add map function

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,13 @@ $(document).ready(function(){
   flavors.forEach(function(flavor){
     $("#flavorList").append("<li>" + flavor + "</li>");
   });
+  // var flavorsMapped = flavors.map(function(flavor){
+  //   return "<li>" + flavor + "</li>";
+  // });
+  //
+  // flavorsMapped.forEach(function(flavor){
+  //   $("#flavorList").append(flavor);
+  // });
+  //
+  // console.log(flavorsMapped)
 });
